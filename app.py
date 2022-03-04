83ca2e129b513c9f95c9b2d09ebfbdb40f452ab5
jake-tolleson <jtolleso@vols.utk.edu>
2022-03-04 12:56:32 -0500

    jake cleaning

--- app.py
+++ app.py
@@ -18,6 +18,49 @@ def draw_graph(id,*args, **kwargs):
             ),
         ])
 
+# Read in files
+# Need pandas version > 1.4 and pyarrow installed
+RATINGS = pd.read_csv('TV_Ratings_onesheet.csv',engine='pyarrow')
+GAMES = pd.read_csv('games_flat_xml_2012-2018.csv',engine='pyarrow')
+CAPACITY = pd.read_csv('capacity.csv',engine='pyarrow')
+
+# clean these duration formats
+GAMES['duration'][679] = '3:07'
+GAMES['duration'][579] = '3:14'
+GAMES['duration'][624] = '3:05'
+GAMES['duration'][773] = '2:11'
+GAMES['duration'][312] = '0:00'
+GAMES['duration'][483] = '3:25'
+GAMES['duration'][491] = '0:00'
+GAMES['duration'][781] = '3:00'
+
+# function to convert duration into minutes
+def duration_minutes(time):
+    hour = np.int16(time.split(':')[0]) * 60
+    minutes = np.int16(time.split(':')[1])
+    return hour+minutes
+
+# use function on duration column
+GAMES['duration_minutes'] = GAMES['duration'].apply(duration_minutes)
+
+# this attendance value is a typo
+GAMES.loc[GAMES['attend'] > 200000,'attend'] = 71004
+
+# merge the 3 datasets
+MERGED = pd.merge(GAMES,RATINGS,how='inner',on='TeamIDsDate')
+# capacity is a custom dataset
+MERGED = pd.merge(MERGED,CAPACITY,on=['homename','stadium'])
+
+# a KPI we will use is perecent of capacity
+MERGED['Percent_of_Capacity'] = MERGED['attend']/MERGED['Capacity']
+
+# compute total tds scored
+MERGED['total_td'] = MERGED[['rush_td_home','pass_td_home','rush_td_vis','pass_td_vis']].sum(axis=1)
+# compute total points scored
+MERGED['total_pts'] = MERGED[['score_home','score_vis']].sum(axis=1)
+# compute point differential
+MERGED['pts_diff'] = np.abs(MERGED['score_home'] - MERGED['score_vis'])
+
 # initialize app
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],
               meta_tags=[
